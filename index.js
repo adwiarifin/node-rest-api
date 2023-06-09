2dfcb812861074b43a1d673be77ddd5f582cfe8d
Adwi Arifin <adwiarifin@gmail.com>
2023-06-09 17:31:16 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// import / dependency
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// body
+const app = express();
+const port = 1234;
+
+app.use(bodyParser());
+
+app.get("/", (req, res) => {
+  console.log(req.query); // {}
+  const value = req.query.query1; // "" / "abc" / undefined
+  console.log(req.headers);
+  const headerValue = req.headers["x-custom"];
+
+  let message = "hello world get";
+  if (value) {
+    message += ` with query1 = ${value}`;
+  }
+  if (headerValue) {
+    message += ` with x-custom = ${headerValue}`;
+  }
+  res.send(message);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("hello world post");
+});
+
+app.put("/", (req, res) => {
+  res.send("hello world put");
+});
+
+app.delete("/", (req, res) => {
+  res.send("hello world delete");
+});
+
+// app.[method](path, (callback function))
+// req, res
+
+// listen
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
