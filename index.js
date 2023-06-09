eed01f92a0a2ca95004593b311c0a2f168cd2fee
Adwi Arifin <adwiarifin@gmail.com>
2023-06-09 19:28:47 +0700

    implement router

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const helmet = require('helmet');
 const bottleneck = require('bottleneck');
 
+const indexRouter = require('./router');
+
 // body
 const app = express();
 const port = 1234;
@@ -12,44 +14,8 @@ const port = 1234;
 app.use(bodyParser());
 app.use(cors());
 app.use(helmet());
-// test something
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
-
-app.get("/", (req, res) => {
-  console.log(req.query); // {}
-  const value = req.query.query1; // "" / "abc" / undefined
-  console.log(req.headers);
-  const headerValue = req.headers["x-custom"];
-
-  let message = "hello world get";
-  if (value) {
-    message += ` with query1 = ${value}`;
-  }
-  if (headerValue) {
-    message += ` with x-custom = ${headerValue}`;
-  }
-  res.send(message);
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("hello world post");
-});
-
-app.put("/", (req, res) => {
-  res.send("hello world put");
-});
-
-app.delete("/", (req, res) => {
-  res.send("hello world delete");
-});
-
-// app.[method](path, (callback function))
-// req, res
+app.use('/', indexRouter);
 
 // listen
 app.listen(port, () => {
